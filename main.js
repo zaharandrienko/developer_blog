dd57d3667fac823830b965958ecf2acca8879e5b
Zahar Andrienko <zaharandrienko88@gmail.com>
2018-10-30 20:46:04 +0200

    add JQuery part

---
+++ main.js
@@ -0,0 +1,10 @@
+$('#toPhotos').on('click', function() {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: images
+      },
+      1000
+    );
+  });
